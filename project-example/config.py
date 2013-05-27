cabeadfdbc7a35f63399e4efd013e4bbdb9f2014
David Eads <davideads@gmail.com>
2013-05-27 12:01:34 -0500

    Ignore secrets.py file and add project-example to production branch for reference

---
+++ project-example/config.py
@@ -0,0 +1,59 @@
+"""
+Google doc configuration. If not provided, no Google doc will be used.
+"""
+
+GOOGLE_DOC = {
+    'key': '0Ak3IIavLYTovdHdPYW9hRE5ockFQdk50WGcyMmprUUE',
+}
+
+
+"""
+Set default context. These variables will be globally available to the template.
+"""
+DEFAULT_CONTEXT = {
+    'title': 'Example project',
+}
+
+"""
+Root URL project will appear at (e.g. http://mydomain.tld/)
+"""
+# URL_ROOT = 'project-example'
+
+"""
+Don't render to static HTML.
+"""
+# DONT_PUBLISH = False
+
+"""
+Don't create JSON for project (default: true)
+"""
+# CREATE_JSON = False
+
+"""
+Uncomment the following lines to provide this configuration file as a Flask
+blueprint.
+"""
+# from flask import Blueprint
+# blueprint = Blueprint('project-example', __name__)
+
+"""
+Example use of flask blueprint to add a template filter.
+"""
+# @blueprint.app_template_filter('example_filter')
+# def example_filter(text):
+#    return text + ' ...suffix.'
+
+"""
+Load secrets. Don't change this unless you know what you're doing.
+"""
+import imp
+import os
+def get_secrets():
+    """ Return a secrets module """
+    root = os.path.dirname(os.path.abspath(__file__))
+    return imp.find_module('secrets', [root])
+
+secrets = imp.load_module('secrets', *get_secrets())
+
+if hasattr(secrets, 'GOOGLE_AUTH'):
+    GOOGLE_DOC.update(secrets.GOOGLE_AUTH)
